1929d1ea1d7fb7a1847d7c5140fb276ea98e8eb7
Bernard Karundeng <bernardkarundeng98@gmail.com>
2025-09-29 00:05:26 +0700

    fix: adjust main.tsx for Vercel build

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
-// --- import yang BENAR sesuai struktur folder kamu ---
-import React, { useState, useCallback } from 'react';
-import ImageUploader from './Component/ImageUploader';
-import GeneratedImage from './Component/GeneratedImage';
-import Loader from './Component/Loader';
-import { SparklesIcon } from './Component/Icons/SparklesIcon';
-import { generateCollage } from './Services/geminiService';
-import { fileToBase64 } from './Utils/fileUtils';
-// --- selesai blok import ---
+function App() {
+  return (
+    <div>
+      <h1>Hello, Virtual Try-On!</h1>
+    </div>
+  )
+}
+
+export default App
