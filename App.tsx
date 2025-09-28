edc9c534b053efb2d91670a34011b1af0da88e63
Bernard Karundeng <bernardkarundeng98@gmail.com>
2025-09-29 00:37:34 +0700

    feat: add basic App component

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
+import React from "react";
+
 function App() {
   return (
-    <div>
-      <h1>Hello, Virtual Try-On!</h1>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🎉 Virtual Try-On Generator 🚀</h1>
+      <p>Deployment berhasil! Kamu sudah bisa mulai edit App.tsx untuk bikin fitur utamamu.</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
